cebb62dfa806fe8a33a4b68476c0b47adc04509e
ghouse <mgmohiuddin2001@gmail.com>
2022-08-13 02:09:31 +0530

    Random Password Generator

---
+++ src.py
@@ -0,0 +1,19 @@
+import string
+from random import choice, shuffle
+
+def randomPasswordGenerator():
+    lower = list(string.ascii_lowercase)
+    upper = list(string.ascii_uppercase)
+    digits = list(string.digits)
+
+    all = lower + upper + digits
+    shuffle(all)
+
+    length = int(input("How long is the password? \n"))
+    password = ''
+
+    for i in range(length):
+        password += choice(all)
+    print(password)
+
+randomPasswordGenerator()
